80c7b3dae08e03ef0ed35c27089a2c8a9d8504f0
Wongboo <Wongbooo@outlook.com>
2026-02-20 02:26:31 +0000

    Deploying to gh-pages from @ Wongboo/me@3e5b2c2b1b9abeb429c500bab94bb92df60041c5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "Wongboo/me",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
