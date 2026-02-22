5500a2e74c3f8398a0947367b9149f67ffacb398
wang-jinbo <Wongbooo@outlook.com>
2026-02-22 07:31:38 +0000

    Deploying to gh-pages from @ wang-jinbo/me@85917c1596ad7ae704189edb613406b025753d75 🚀

--- assets/js/open-panel-analytics-setup.js
+++ assets/js/open-panel-analytics-setup.js
@@ -4,7 +4,7 @@ window.op =
     (window.op.q = window.op.q || []).push(args);
   };
 window.op("init", {
-  clientId: "",
+  clientId: "3dc10551-2fbd-45f3-a1b4-59558046b482",
   trackScreenViews: true,
   trackOutgoingLinks: true,
   trackAttributes: true,
